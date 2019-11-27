72aa0f210f1450b5a80dac67157dbf303d9242e4
ChaosLime <ec2-user@ip-172-31-24-239.us-west-2.compute.internal>
2019-11-27 02:32:04 +0000

    set up to merge

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,6 +1,63 @@
- $(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
+$(document).ready(function () {
+    var sideActive = false;
+    var isProductAdded = false;           
+
+    $('#sidebarCollapse').on('click', function () {
+        if(isProductAdded){
+            if(!sideActive){
+            sideActive = true;
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('active');
+            }
+            else if(sideActive){
+                sideActive = false;
                 $('#sidebar').toggleClass('active');
                 $(this).toggleClass('active');
-            });
-        });
+            }
+        }
+        else {
+              alert("Please add items to cart before viewing.");
+
+        }
+    });
+    
+    let alreadyAdded = false;
+    $('#btn-add').on('click', function () {
+        if(!sideActive){
+            if(!alreadyAdded){
+                sideActive = true;
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('active');
+            isProductAdded = true;
+            console.log("add product to cart.");
+            addToCart("1");
+            alreadyAdded = true;
+            }
+            else{
+                  alert("Already in cart, please review in Checkout.");
+            }
+            
+        }
+        else{
+            alert("Already in cart, please review in Checkout.");
+        }
+     });
+
+    function addToCart(x){
+        if(x == 1){
+            document.getElementById("productInCart").innerHTML=
+        '<img src="./img/testShoe.jpg"/>';
+        //document.getElementById("quantity").innerHTML=
+        //quantity;
+        }
+        else {
+            alert("product not added to cart");
+        }
+        
+        
+
+    }
+
+    
+    
+});
