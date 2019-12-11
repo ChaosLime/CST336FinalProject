77a031642609793cf581ac465ec91e0cecd2b690
ChaosLime <ec2-user@ip-172-31-24-239.us-west-2.compute.internal>
2019-12-11 08:35:54 +0000

    made valuation checks into admin area

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get("/login", function(req, res) {
     res.render("login.html");
 });
 
+app.get("/valuations", function(req, res) {
+    res.render("valuations.html");
+});
+
 app.get("/cart.html", function(req, res) {
     var conn = tools.createConnection();
     var sql = "SELECT cart.username, cart.sequence, cart.quantity_in_cart, inventory.model, " +
@@ -107,6 +111,53 @@ app.get("/db/displayInventory", async function(req, res) {
     });
 
 }); //display Inventory
+
+
+//gets all from inventory
+app.get("/db/getValuation", async function(req, res) {
+    var conn = tools.createConnection();
+    var sql;
+    var sqlParams;
+
+    sql = "CALL getInventoryValuation(?);";
+    sqlParams = [req.query.userInput];
+
+    conn.connect(function(err) {
+
+        if (err) throw err;
+
+        conn.query(sql, sqlParams, function(err, result) {
+            if (err) throw err;
+            conn.on('error', function(err) {
+                console.log(err.code); // 'ER_BAD_DB_ERROR'
+            });
+            if (result == " ") {
+                alert("empty result.");
+                conn.end();
+            }
+
+            /*This block below is intended to clean up and modify the results from the database
+            as a string and parse is accordingly. The counter limits the amount of results to be displayed,
+            if omited in the split method, will return all results of that query.
+            For multiple result, it is simply seperated by a ",". Could not quite parse nicely with
+            JSON from the database therefore was stringified and adjusted.
+            */
+            var stringifiedResult = JSON.stringify(result);
+            var splitResult = stringifiedResult.split(",RowDataPacket ");
+            var replacedString = splitResult.toString().replace(/[^a-zA-Z0-9[]_:.{},\/"]/g, ' ').replace(/  +/g, ' ');
+            //sets up data to be parsed and displayed on screen
+            //console.log(replacedString);
+            res.send(replacedString); //This is the correct method to use to pass information back
+            conn.end();
+        });
+
+    });
+
+}); //display Inventory
+
+
+
+
 
 //gets all from inventory
 app.get("/db/insertIntoCart", async function(req, res) {
