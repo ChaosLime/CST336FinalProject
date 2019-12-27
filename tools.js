9b774aa58946d65f042728be73d96778c197d7d7
ChaosLime <ec2-user@ip-172-31-24-239.us-west-2.compute.internal>
2019-12-27 06:41:29 +0000

    changed db to cleardb

--- tools.js
+++ tools.js
@@ -8,10 +8,10 @@ module.exports = {
       */
     createConnection: function(){
          var conn = mysql.createConnection({
-        host: 'cst336db.space',
-        user: 'cst336_dbUser030',
-    password: 'qq0mon',
-    database: 'cst336_db030'
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'bc0ca7ea703d0d',
+    password: 'c8af3bcc',
+    database: 'heroku_35b42c01aef0b09'
     });
     return conn;
 
